6c14e92af738e2ca12aa600262f0be5fc524e3ca
Samuel Abedu <samwisegh@gmail.com>
2021-09-04 09:12:49 +0000

    initial commit

---
+++ Hubtel.eCommerce.Cart.Api/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hubtel.eCommerce.Cart.Api.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
---
+++ Hubtel.eCommerce.Cart.Api/Models/ShoppingCartContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hubtel.eCommerce.Cart.Api.Models
+{
+    public class ShoppingCartContext:DbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
